33f66613c771f7993e57d6d8ca4d011b07d937a3
stexus <massimo.tseng@gmail.com>
2021-03-13 19:25:17 -0800

    rofi selection in mpv

--- helpers/update_ep.lua
+++ helpers/update_ep.lua
@@ -6,8 +6,11 @@ local msg = require('mp.msg')
 -- read JSON file if exists
 --      check folder where media is being played. grab ep and increment
 -- otherwise create one
-local curr_list, title, curr_ep 
-local medialist = os.getenv('HOME') .. '/.medialist.json'
+-- create curr_ep object
+local curr = {}
+local curr_list, title, curr_ep, curr_id
+--local medialist = os.getenv('HOME') .. '/.medialist.json'
+local medialist = '/mnt/misc-ssd/Coding/Projects/media-tracker/medialist.json'
 
 --allow for soft links
 local mediadir_name = 'Anime'
@@ -17,11 +20,14 @@ local dir = utils.getcwd()
 
 
 --processing functions--
-local function subprocess(command, stdin)
+local function subprocess(command, stdout, stdin)
+    if stdout == nil then
+        stdout = true
+    end
     return mp.command_native({
             name = 'subprocess',
             playback_only = false,
-            capture_stdout = true,
+            capture_stdout = stdout,
             stdin_data = stdin,
             args = command
         })
@@ -33,9 +39,10 @@ local function get_ep()
     local commands = {'find', dir..title, '-type', 'l,f', '-name', '*.mkv'}
     local result = subprocess(commands)
     local line
-    if result.status == 0 then 
-        local sorted = subprocess({'sort'}, result.stdout)
-        line = subprocess({'grep', '-Fn', filename}, sorted.stdout)
+    if result.status == 0 then
+        --changed to named arguments
+        local sorted = subprocess({'sort'}, true, result.stdout)
+        line = subprocess({'grep', '-Fn', filename}, true, sorted.stdout)
     end
     if line.status == 0 then
         local end_index = line.stdout:find(':')
@@ -45,12 +52,76 @@ local function get_ep()
     return 0
 end
 
+--for debugging
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
+local function entry_ep(entry) return entry[1] end
+local function entry_id(entry) return entry[2] end
+local function extract_id()
+    local anilist_entries = curr_list['anilist']
+    local id = -1
+    --msg.info(dump(anilist_entries))
+    for _, entry in ipairs(anilist_entries[title]) do
+        --msg.info(dump(entry))
+        --msg.info(curr_ep)
+        if entry_ep(entry) <= curr_ep then
+            id = entry_id(entry)
+        end
+    end
+    --msg.info(entry_id)
+    return id
+end
+
 local function extract_title(subdirs)
     local i, j = string.find(subdirs, '/')
     if not i then return subdirs end
     mp.osd_message(subdirs:sub(i + 1, #subdirs), 2)
     return subdirs:sub(0, i - 1)
 end
+
+local function add_anilist_entry(id)
+    curr_id = id
+    if not curr_list['anilist'] then curr_list['anilist'] = {} end
+    local anilist_entries = curr_list['anilist']
+    if not anilist_entries[title] then
+        anilist_entries[title] = {}
+    end
+    local new_entry = {curr_ep, tonumber(id)}
+    for i, entry in ipairs(anilist_entries[title]) do
+        if entry_ep(entry) == curr_ep then
+            anilist_entries[title][i] = new_entry
+            return
+        end
+    end
+    table.insert(anilist_entries[title], new_entry)
+    --msg.info(dump(anilist_entries))
+end
+
+
+
+local function rofi_selection()
+    local tmp_name = os.tmpname()
+    subprocess({'selector', tmp_name}, true, '')
+    local tmp = io.open(tmp_name, 'r')
+    if tmp then
+        local id = tmp:read('*a')
+        io.close(tmp)
+        os.remove(tmp_name)
+        add_anilist_entry(tonumber(id))
+    end
+end
+
+
 ------------------------------------------------------------------
 
 
@@ -64,7 +135,6 @@ JSON.saveTable = function(path, v)
 end
 
 JSON.loadTable = function(path)
-    local contents = ""
     local myTable = {}
     local file = io.open(path,"r")
     if file then
@@ -104,6 +174,10 @@ local function complete_ep(ep)
     curr_list[title] = ep or curr_ep
     JSON.saveTable(medialist, curr_list)
     --send api request to anilist
+    if curr_id > 0 then
+        --send request
+        msg.info('sending request to anilist')
+    end
     mp.osd_message('Marked completed: '..curr_list[title], 1)
 end
 -------------------------
@@ -138,6 +212,7 @@ local function init()
     end
     curr_ep = get_ep()
     curr_list = JSON.loadTable(medialist)
+    curr_id = extract_id()
     --set variable to update in anilist
 end
 
@@ -160,7 +235,8 @@ mp.add_forced_key_binding('ctrl+w', 'set_ep_prev', function()
 end)
 
 --mark current completed
-mp.add_forced_key_binding('ctrl+shift+w', 'set_ep_curr', function() complete_ep(curr_ep) end)
+mp.add_forced_key_binding('ctrl+shift+w', 'set_ep_Surr', function() complete_ep(curr_ep) end)
+mp.add_forced_key_binding('alt+a', 'rofi-blocks', rofi_selection)
 
 
 
