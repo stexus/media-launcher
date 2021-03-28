212934dc6178be78f0ff2e8465b772134b694949
stexus <massimo.tseng@gmail.com>
2021-03-27 21:30:29 -0700

    added anilist api tests

--- tests/anilist_test.py
+++ tests/anilist_test.py
@@ -9,12 +9,20 @@ class TestAnilistAPI(unittest.TestCase):
         self.assertEqual(api._get_viewer(), self.viewer)
     def test_get_progress_not_in_list(self):
         horimiya = 124080
-        self.assertEqual(api._get_progress(self.viewer, horimiya), 0)
+        self.assertEqual(api._get_progress(self.viewer, horimiya), 1)
     def test_get_progress_in_list(self):
         aot = 16498
         self.assertEqual(api._get_progress(self.viewer, aot), 25)
     def test_is_complete(self):
         aot = 16498
         self.assertEqual(api._is_complete(aot, 25), True)
+    def test_update_entry(self):
+        tonikawa = 116267
+        self.assertEqual(api.update_entry(tonikawa), 3)
+    def test_add_entry(self):
+        horimiya = 124080
+        self.assertEqual(api.update_entry(horimiya), 1)
+
+        
 if __name__ == '__main__':
     unittest.main()
